e49f6d6bf0112d377a817094aac62867855158b0
Isa Randra <isarandra@yahoo.com>
2022-12-21 22:45:25 +0700

    add scheduler for challenge 8

--- src/main/java/com/codewithisa/filmservice/FilmServiceApplication.java
+++ src/main/java/com/codewithisa/filmservice/FilmServiceApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @OpenAPIDefinition(
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 		}
 )
 @SpringBootApplication
+@EnableScheduling
 public class FilmServiceApplication {
 
 	public static void main(String[] args) {
